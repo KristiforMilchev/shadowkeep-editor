6ed52a21a798a71d8c8ee19ee50f1a7b7d9414c0
Kristifor Milchev <km@airzen.io>
2023-07-05 19:31:57 +0300

    Implementing a quoutes command

--- lib/document.dart
+++ lib/document.dart
@@ -362,6 +362,9 @@ class Document {
       case 'ctrl+[':
         createList();
         break;
+      case 'ctrl+"':
+        encapsulate(keepAnchor: true);
+        break;
     }
   }
 
@@ -485,5 +488,49 @@ class Document {
     }
 
     lines[cursor.line].text = left + right;
+  }
+
+  void encapsulate({required bool keepAnchor}) {
+    if (!cursor.hasSelection()) {
+      checkQuoutesLine();
+      return;
+    }
+
+    checkQuoutesSelection();
+  }
+
+  void checkQuoutesLine() {
+    var currentLine = lines[cursor.line].text;
+    if (currentLine.characters.first == '"') {
+      var noQuoutes = lines[cursor.line]
+          .text
+          .substring(1, lines[cursor.line].text.length - 1);
+
+      lines[cursor.line].text = noQuoutes;
+    } else {
+      // ignore: prefer_single_quotes
+      lines[cursor.line].text = "\"${lines[cursor.line].text}\"";
+    }
+  }
+
+  void checkQuoutesSelection() {
+    if (!cursor.hasSelection()) return;
+
+    var left = lines[cursor.line].text.substring(0, cursor.column);
+    var right = lines[cursor.line]
+        .text
+        .substring(cursor.anchorColumn, lines[cursor.line].text.length);
+    var quouteContent =
+        lines[cursor.line].text.substring(cursor.column, cursor.anchorColumn);
+
+    if (quouteContent.characters.first == '"') {
+      var noQuoutes = quouteContent.substring(0, quouteContent.length - 1);
+
+      // ignore: prefer_single_quotes
+      lines[cursor.line].text = "$left $noQuoutes $right";
+    } else {
+      // ignore: prefer_single_quotes
+      lines[cursor.line].text = "$left \"$quouteContent\" $right";
+    }
   }
 }
